9c742b663d2f8023af7ef0af6d14a23033807813
ineilzhang <zyf13682501215@gmail.com>
2017-08-11 09:40:25 +0800

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Neil Zhang on 2017/8/11.
+//  Copyright © 2017年 Neil Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
